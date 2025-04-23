8df04bc2d16d442a901859c08c27c0d4c22dce7c
Petr Čechura <petr.cechura@reporting.cz>
2025-04-23 21:15:41 +0200

    update install script

--- scripts/install-powerbi-connector.ps1
+++ scripts/install-powerbi-connector.ps1
@@ -1,10 +1,19 @@
 $documentDir = [Environment]::GetFolderPath('MyDocuments')
 $pbiDir = "$documentDir\Microsoft Power BI Desktop\Custom Connectors"
 
-if (!(Test-Path -Path $pbiDir)) {
-	New-Item -ItemType Directory -Path $pbiDir -Force | Out-Null
+try {
+	if (!(Test-Path -Path $pbiDir)) {
+		New-Item -ItemType Directory -Path $pbiDir -Force | Out-Null
+	}
+	
+	$sourceUrl = "https://github.com/reporting-cz/reporting-connectors/raw/refs/heads/main/powerbi/dist/reporting-powerbi.mez"
+	$destinationPath = "$pbiDir\reporting-powerbi.mez"
+	Invoke-WebRequest -Uri $sourceUrl -OutFile $destinationPath
+	
+	Write-Host "Power BI connector installed successfully at $destinationPath" -ForegroundColor Green
+} catch {
+	$ErrorMessage = $_.Exception.Message
+	Write-Host "Error: $ErrorMessage" -ForegroundColor Red
 }
 
-$sourceUrl = "https://github.com/reporting-cz/reporting-connectors/raw/refs/heads/main/powerbi/dist/reporting-powerbi.mez"
-$destinationPath = "$pbiDir\reporting-powerbi.mez"
-Invoke-WebRequest -Uri $sourceUrl -OutFile $destinationPath
+
